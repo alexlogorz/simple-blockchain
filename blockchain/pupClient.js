dca1a39c95e56c2338a5c0212e703d908d9eb137
alex logorz <alex.logorz@gmail.com>
2022-02-16 23:43:46 -0500

    implemented a consensus algorithm

--- blockchain/pupClient.js
+++ blockchain/pupClient.js
@@ -5,18 +5,25 @@ const server = new WS.Server({ port: PORT });
 const MY_ADDRESS = `ws://localhost:${PORT}`;
 const peers = [];
 const handshakes = [];
+let hasVoted = false;
+const surveyResults = { accepted: 0, rejected: 0 };
 
 console.log('Listening on PORT', PORT);
 
 server.on('connection', async (socket, req) => {
     socket.on('message', message => {
         const _message = JSON.parse(message);
-        console.log(_message);
-        if(_message.type === 'handshake') {
-            connect(_message.address);
-        }
-        else if(_message.type === 'msg') {
-            console.log(_message.value);
+        switch(_message.type) {
+            case 'HANDSHAKE':
+                console.log(_message);
+                connect(_message.address);
+                break;
+            case 'SURVEY':
+                castVote(_message);
+                break;
+            case 'VOTE':
+                appendSurveyResults(_message.accepted);
+                break;
         }
     });
 });
@@ -25,10 +32,34 @@ async function connect(address) {
     if(!handshakes.find(peer => peer.address === address)) {
         const socket = new WS(address);
         socket.on('open', () => {
-            handshakes.push({socket, address});
-            socket.send(JSON.stringify({type: 'handshake', address: MY_ADDRESS}));
+            handshakes.push({ socket, address });
+            socket.send(JSON.stringify({ type: 'HANDSHAKE', address: MY_ADDRESS }));
         });
     }
+}
+
+function castVote(message) {
+    const { number, address } = message;
+    if(!hasVoted) {
+        if(number >= 0)
+            sendMessage({ type: 'VOTE', accepted: true }, address);
+        else
+            sendMessage({ type: 'VOTE', accepted: false }, address);
+        hasVoted = true;
+    }
+}
+
+function appendSurveyResults(accepted) {
+    if(accepted)
+        surveyResults.accepted += 1;
+    else
+        surveyResults.rejected += 1;
+}
+
+function sendMessage(message, address) {
+    handshakes.forEach(peer => {
+		if(peer.address === address) peer.socket.send(JSON.stringify(message));
+	})
 }
 
 function broadcast(message) {
@@ -37,8 +68,15 @@ function broadcast(message) {
 	})
 }
 
+function surveyTimer(delay) {
+    setTimeout(() => {
+        console.log('Survey results', surveyResults);
+    }, delay);
+}
+
 peers.forEach(peer => connect(peer));
 
 setTimeout(() => {
-    broadcast({type: 'msg', value: 'hello world'});
-}, 12000);
+    broadcast({ type: 'SURVEY', address: MY_ADDRESS, number:-1 });
+    surveyTimer(10000);
+}, 10000);
--- blockchain/pupClient3.js
+++ blockchain/pupClient3.js
@@ -5,18 +5,25 @@ const server = new WS.Server({ port: PORT });
 const MY_ADDRESS = `ws://localhost:${PORT}`;
 const peers = ['ws://localhost:3000', 'ws://localhost:3001'];
 const handshakes = [];
+let hasVoted = false;
+const surveyResults = { accepted: 0, rejected: 0 };
 
 console.log('Listening on PORT', PORT);
 
 server.on('connection', async (socket, req) => {
     socket.on('message', message => {
         const _message = JSON.parse(message);
-        console.log(_message);
-        if(_message.type === 'handshake') {
-            connect(_message.address);
-        }
-        else if(_message.type === 'msg') {
-            console.log(_message.value);
+        switch(_message.type) {
+            case 'HANDSHAKE':
+                console.log(_message);
+                connect(_message.address);
+                break;
+            case 'SURVEY':
+                castVote(_message);
+                break;
+            case 'VOTE':
+                appendSurveyResults(_message.accepted);
+                break;
         }
     });
 });
@@ -25,16 +32,48 @@ async function connect(address) {
     if(!handshakes.find(peer => peer.address === address)) {
         const socket = new WS(address);
         socket.on('open', () => {
-            handshakes.push({socket, address});
-            socket.send(JSON.stringify({type: 'handshake', address: MY_ADDRESS}));
+            handshakes.push({ socket, address });
+            socket.send(JSON.stringify({ type: 'HANDSHAKE', address: MY_ADDRESS }));
         });
     }
+}
+
+function castVote(message) {
+    const { number, address } = message;
+    if(!hasVoted) {
+        if(number >= 0)
+            sendMessage({ type: 'VOTE', accepted: true }, address);
+        else
+            sendMessage({ type: 'VOTE', accepted: false }, address);
+        hasVoted = true;
+    }
+}
+
+function appendSurveyResults(accepted) {
+    if(accepted)
+        surveyResults.accepted += 1;
+    else
+        surveyResults.rejected += 1;
+}
+
+function sendMessage(message, address) {
+    handshakes.forEach(peer => {
+		if(peer.address === address) peer.socket.send(JSON.stringify(message));
+	})
 }
 
 function broadcast(message) {
 	handshakes.forEach(peer => {
 		peer.socket.send(JSON.stringify(message));
 	})
+}
+
+function surveyTimer(delay) {
+    const { accepted, rejected } = surveyResults;
+    setTimeout(() => {
+        const results = (accepted > rejected) ? true : false;
+        console.log('Survey results', results);
+    }, delay);
 }
 
 peers.forEach(peer => connect(peer));
